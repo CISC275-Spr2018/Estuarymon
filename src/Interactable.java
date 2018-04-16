ad10b9f513bb21dfb9c97ff70d9d840a3bd495c6
Zack Klodnicki <klodzack@udel.edu>
2018-04-16 18:37:12 -0400

    Added constructor, getters & move()

--- src/Interactable.java
+++ src/Interactable.java
@@ -3,8 +3,36 @@ import java.awt.Rectangle;
 public class Interactable {
 	int xLocation;
 	int yLocation;
-	int width;
-	int height;
+	final int width;
+	final int height;
+
+	public Interactable(int xloc, int yloc, int width, int height) {
+		this.xLocation = xloc;
+		this.yLocation = yloc;
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getXLocation() {
+		return this.xLocation;
+	}
+
+	public int getYLocation() {
+		return this.yLocation;
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public void moveTo(int x, int y) {
+		this.xLocation = x;
+		this.yLocation = y;
+	}
 
 	public Rectangle getCollisionRect() {
 		return new Rectangle(xLocation, yLocation, width, height);
