07408532160bd8b82fa066695ca70c34f36b60ff
Matthew <gargamat@udel.edu>
2018-05-06 20:52:14 -0400

    Added Receptacle Function to existing code

--- src/MapObjects/Receptacle.java
+++ src/MapObjects/Receptacle.java
@@ -1,26 +1,37 @@
 package MapObjects;
 import Player.Player;
-
+/**Class of the Receptacles in which the Litter objects are placed into. 
+ * A subclass of Interactable
+ * 
+ * 
+ * 
+ * 
+ *
+ */
 public class Receptacle extends Interactable{
-	
 	ReceptacleType rType;
-	public Receptacle(int xLoc, int yLoc, int rWidth, int rHeight, ReceptacleType rType) {
-		super(xLoc, yLoc, rWidth, rHeight);
+	public static int trashYpos = 450;
+	public static int recyclingYpos = 680;
+	/** Constructor for Receptacle objects. The x coordinate is set to 0, the y coordinate is set according to the type of receptacle.
+	 * The height and width are determined. 
+	 *
+	 */
+	public Receptacle(int rWidth, int rHeight, ReceptacleType rType) {
+		super(0, trashYpos + rType.ordinal()*(recyclingYpos - trashYpos), rWidth, rHeight);
 		this.rType = rType;
 		this.setRelativeCollisionRect(0, 0, rWidth, rHeight);
 	}
+	/**Method that removes Litter from the player object 
+	 * @param p the player Object interacting with the receptacle 
+	 */
 	public void takeLitter(Player p) {
-		//System.out.println("Receptable "+ rType.getName() + " take litter!");
-		// You can do something like:
-		/*
-		if(p.getLitterType().isInstance(this)) {
-			// Successfully matched to trash or recycle!
-		} else {
-			// Deposited in wrong bin!
-		}
-		*/
+		Player.hasLitter = false;
 	}
-	
+	/** Method that returns the ReceptacleType
+	 *
+	 * 
+	 * @return ReceptacleType of the Receptacle
+	 */
 	public ReceptacleType getType() {
 		return this.rType;
 	}
